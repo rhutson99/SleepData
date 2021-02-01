ce06727cd65d839c05e13ab5fb08e860cb8510ee
ryanhutson99 <ryanhutson99@gmail.com>
2021-02-01 14:23:52 -0600

    Determine start/end dates

--- Program.cs
+++ Program.cs
@@ -15,13 +15,20 @@ namespace SleepData
 
             if (resp == "1")
             {
-                                // create data file
+                 // create data file
 
                  // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
                 int weeks = int.Parse(Console.ReadLine());
 
+                 // determine start and end date
+                DateTime today = DateTime.Now;
+                // we want full weeks sunday - saturday
+                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                // subtract # of weeks from endDate to get startDate
+                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+                Console.WriteLine(dataDate);
             }
             else if (resp == "2")
             {
