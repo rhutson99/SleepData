4eda5d09becf8349944b17787fc1420ec318ed50
ryanhutson99 <ryanhutson99@gmail.com>
2021-02-01 14:18:53 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SleepData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+
+            if (resp == "1")
+            {
+                // TODO: create data file
+            }
+            else if (resp == "2")
+            {
+                // TODO: parse data file
+
+            }
+        }
+    }
+}
